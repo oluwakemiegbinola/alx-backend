46eab929dd9949b7a328d5f58b2a8026df4cddbb
oluwakemiegbinola <oluwkemiegbinola@gmail.com>
2024-05-14 07:05:01 +0000

    il8n

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def index():
+    """view function for root route
+
+    Returns:
+        html: homepage
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
